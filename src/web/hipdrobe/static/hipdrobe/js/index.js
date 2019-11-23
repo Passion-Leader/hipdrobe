0b4a74d9ec84916565684589a2805b3348e87eb3
codeorchord <codeorchord@gmail.com>
2019-11-24 02:56:54 +0900

    carousel 적용; svg logo 적용; login section 추가; dummy contents 추가;

---
+++ src/web/hipdrobe/static/hipdrobe/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // Index page는 로고 흰색
+    fillLogoH();
+    fillLogoSubH();
+
+    // Index page는 페이지 제목 없음
+    setTitle("");
+
+    // Carousel 밑의 컨텐츠 시작 위치 맞추기
+    fitBottom();
+
+});
+
+window.onresize = function(event) {
+    fitBottom();
+};
+
