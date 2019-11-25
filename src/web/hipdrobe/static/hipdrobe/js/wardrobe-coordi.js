635e97e9086a2894b61e96e79905fc6abc3c37b8
codeorchord <codeorchord@gmail.com>
2019-11-25 20:36:43 +0900

    서브 항목 용어 변경; 중복 폰트 제거;

--- src/web/hipdrobe/static/hipdrobe/js/wardrobe-coordi.js
+++ src/web/hipdrobe/static/hipdrobe/js/wardrobe-coordi.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     fillLogoSubB();
 
     // 제목 및 액티브 효과 적용
-    setTitle(["나만의 옷장 :", "being 코디네이터"]);
+    setTitle(["나만의 옷장 :", "코디 작성"]);
     setActive(["main-ln-wardrobe", "ln-coordi"]);
 
 });
