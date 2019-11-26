6db2b4f52157caf814087713824197f1c2a24bf3
aksel26 <kevinxkim2023@gmail.com>
2019-11-26 15:45:17 +0900

    migrate ; * settings.py - extensions, imagekit 모듈 못찾아 runserver가 안돼서 일단 주석처리 해놓았습니다.

--- src/web/hipdrobe/models.py
+++ src/web/hipdrobe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userid = models.CharField(max_length=50)
+    userpw = models.CharField(max_length=20)
+    nick = models.CharField(max_length=12)
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField()
+    logged_at=models.DateTimeField(auto_now=True)
+    level_point=models.IntegerField()
+    level=models.IntegerField()
+    item_top=models.IntegerField()
+    item_bottom=models.IntegerField()
+    item_shoes=models.IntegerField()
+
