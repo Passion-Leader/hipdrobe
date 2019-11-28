5824af296e652a2ae3f750d57a74a8e2be0bce8e
aksel26 <kevinxkim2023@gmail.com>
2019-11-28 21:59:25 +0900

    비밀번호 중복

--- src/web/hipdrobe/views.py
+++ src/web/hipdrobe/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import User
-
+import simplejson as json
+from django.http import HttpResponse
 
 # Create your views here.
 # -----------------------------------------------------------------------------
@@ -29,30 +30,26 @@ def stat(request):
 def regist(request):
         if request.method=="POST":
             
-            userid1=request.POST["userid"]
+            userid1=request.POST["userid"],
             
-            pw=request.POST["userpw1"]
+            pw=request.POST["userpw1"],
             
-            nickname=request.POST["nick"]
+            nickname=request.POST["nick"],
             
-            gender1=request.POST.get('gender')
+            gender1=request.POST.get('gender'),
             
             if gender1=='male':
                 gender1=1
             else:
                 gender1=2
             
-            created=request.POST.get('created_at')
-            updated=request.POST.get('updated_at')
-            log=request.POST.get('logged_at')
-            levelpoint=request.POST.get('level_point')
-            lev=request.POST.get('level')
-            top=request.POST.get('count_top')
-            bottom=request.POST.get('count_bottom')
-            shoes=request.POST.get('count_shoes')
+            
             new_user=User(userid=userid1,userpwd=pw,nick=nickname,gender=gender1)            
+            
             new_user.save()            
             return redirect("hipdrobe:index")
         else: 
             return render(request,'hipdrobe/regist.html')
         
+
+
