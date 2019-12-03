a9400bc389951df61ec6b20e5126ce4b94de5f84
codeorchord <codeorchord@gmail.com>
2019-12-03 17:42:16 +0900

    코디 작성 페이지 draft 작성중

--- src/web/hipdrobe/static/hipdrobe/js/char-win.js
+++ src/web/hipdrobe/static/hipdrobe/js/char-win.js
@@ -7,6 +7,10 @@
 
 var g_moveables = []
 
+$(document).ready(function(){
+    _setPartsHeight();
+});
+
 function enableDnD() {
     const elems = document.querySelectorAll('.moveable');
     const options = { constrain: true };
@@ -22,6 +26,41 @@ function disableDnD() {
     }
     g_moveables = [];
 }
+
+
+/*-----------------------------------------------------------------------------
+ * 개별 코딩 해 놓고 추후 결정되면 공통사항 묶어서 다시 코딩
+ */
+function _setPartsHeight() {
+    // Div
+    const elemDiv = $('#id-div-char-win')
+    let divWidth = parseInt(elemDiv.css('width'));
+    elemDiv.css('height', `${divWidth}px`);
+
+    // Head
+    const elemHead = $('#id-coord-head')
+    let headWidth = parseInt(elemHead.css('width'));
+    elemHead.css('height', `${headWidth}px`);
+
+    // Top-1
+    const elemTop1 = $('#id-coord-top1')
+    let top1Width = parseInt(elemTop1.css('width')) * 1.4;
+    elemTop1.css('height', `${top1Width}px`);
+
+    // Top-2
+    const elemTop2 = $('#id-coord-top2')
+    let top2Width = parseInt(elemTop2.css('width')) * 1.4;
+    elemTop2.css('height', `${top2Width}px`);
+
+    // Pants-1
+    const elemPants1 = $('#id-coord-pants1')
+    let pants1Width = parseInt(elemPants1.css('width')) * 1.4;
+    elemPants1.css('height', `${pants1Width}px`);
+}
+
+window.onresize = function(event) {
+    _setPartsHeight();
+};
 
 
 /* test code
