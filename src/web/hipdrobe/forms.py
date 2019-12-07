fe31f23b1eb37496d921ec07f4ce518433e40f70
aksel26 <kevinxkim2023@gmail.com>
2019-12-08 04:44:11 +0900

    Revert "Revert "email계정으로 로그인 등은 model 커스텀 migration 때문에 user계정 가입,로그인 가능하게 수정해놨습니다.""
    This reverts commit 3f1b5936699f808629e03fa1730054619e1018aa.

--- src/web/hipdrobe/forms.py
+++ src/web/hipdrobe/forms.py
@@ -9,13 +9,21 @@ class UserForm(forms.ModelForm):
         'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
         'email': forms.EmailInput(attrs={'class': 'form-control'}),
         'password' : forms.PasswordInput(attrs={'class': 'form-control'}),
+        
     }
     labels = {
         'username': '닉네임',
         'email': '이메일',
-        'password': '패스워드'
+        'password': '패스워드',
+        
     }
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
 # 글자수 제한
 def __init__(self, *args, **kwargs):
     super(UserForm, self).__init__( *args, **kwargs)
-    self.fields['username'].widget.attrs['maxlength'] = 15
+    self.fields['email'].widget.attrs['maxlength'] =59
+    
