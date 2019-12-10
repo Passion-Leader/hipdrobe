ed6e0feb33a41a556cc98dbb083986b0c086704f
codeorchord <codeorchord@gmail.com>
2019-12-10 20:10:59 +0900

    나만의 옷장: 옷 추가 시 부위/카테고리 정보 안 뜨고 업로드 동작 안 되는 문제 수정

--- src/web/hipdrobe/static/hipdrobe/js/apis-updateitem.js
+++ src/web/hipdrobe/static/hipdrobe/js/apis-updateitem.js
@@ -1,5 +1,5 @@
 /*
- * apis-additem.js
+ * apis-updateitem.js
  */
 
 
@@ -9,8 +9,8 @@
  * 필수항목 validator를 구성
  */
 $('document').ready(function(){
-    _setUploadTrigger();
-    _makeValidator();
+    _setUploadTrigger($('#id-btn-updateitem'), $('#id-form-updateitem'));
+    _makeValidator_clothes_update();
 });
 
 
@@ -20,68 +20,15 @@ $('document').ready(function(){
 function openUpdateItem(event) {
     _eraseOption('#id-updateitem-cate1')
     _eraseOption('#id-updateitem-cate2')
-    _eraseErrorLabel();
+    eraseErrorLabel($('#id-modal-updateitem'));
     
     $.ajax({
         type: "GET",
         url: "/apis/parts/",
         success: function (data) {
-            _clearModal()
-            _addOption('#id-updateitem-part', data['parts'])
+            _clearModal($('#id-modal-updateitem'), $('#id-updateitem-id'));
+            _addOption('#id-updateitem-part', data['parts']);
             $('#id-modal-updateitem').modal('toggle');
-        },
-        error: function (e) {
-            console.log("ERROR : ", e);
-            alert("fail");
-        }
-   });
-}
-
-
-/*-----------------------------------------------------------------------------
- * 부위가 변경되면 카테고리1의 항목값을 DB에서 받아와 채워줌
- */
-function onPartChange() {
-    part_name = $('#id-updateitem-part option:selected').val()
-    if (part_name == "") {
-        _eraseOption('#id-updateitem-cate1')
-        _eraseOption('#id-updateitem-cate2')
-        return;
-    }      
-
-    $.ajax({
-        type: "GET",
-        url: "/apis/cate1/",
-        contentType: "application/json",
-        data: {part: part_name},
-        success: function (data) {
-            _addOption('#id-updateitem-cate1', data['cate1'])
-        },
-        error: function (e) {
-            console.log("ERROR : ", e);
-            alert("fail");
-        }
-   });
-}
-
-
-/*-----------------------------------------------------------------------------
- * 카테고리1이 변경되면 카테고리2의 항목값을 DB에서 받아와 채워줌
- */
-function onCate1Change() {
-    cate1_name = $('#id-updateitem-cate1 option:selected').val()
-    if (cate1_name == "") {
-        _eraseOption('#id-updateitem-cate2')
-        return;
-    }
-        
-    $.ajax({
-        type: "GET",
-        url: "/apis/cate2/",
-        contentType: "application/json",
-        data: {cate1: cate1_name},
-        success: function (data) {
-            _addOption('#id-updateitem-cate2', data['cate2'])
         },
         error: function (e) {
             console.log("ERROR : ", e);
@@ -123,11 +70,9 @@ function onCate1Change() {
                 $("#detail_modal").load();
                 
                 // updateitem페이지 초기화
-                _clearModal();
+                _clearModal($('#id-modal-updateitem'), $('#id-updateitem-id'));
 
-              })
-            
-          
+            })
         },
         error: function (e) {
             console.log("ERROR : ", e);
@@ -135,83 +80,6 @@ function onCate1Change() {
             $('#id-btn-updateitem').prop("disabled", false);
             disableLoading();
         }
-   });
- }
-
- 
-/*-----------------------------------------------------------------------------
- * target으로 주어진 select-box에 data로 주어진 항목들을 채움
- */
-function _addOption(target, data) {
-    var select = $(target).html("");
-    select.append("<option value='' selected>선택하세요</option>");
-
-    for(let i = 0; i < data.length; i++) {
-        opt = data[i];
-        select.append("<option value='" + opt + "'>" + opt + "</option>");
-    }
-}
-
-
-/*-----------------------------------------------------------------------------
- * target으로 주어진 select-box의 모든 항목을 지우고 안내 문구만 남김
- */
-function _eraseOption(target) {
-    let select = $(target).html("");
-    select.append("<option selected>선택하세요</option>");
-}
-
-
-/*-----------------------------------------------------------------------------
- * 의상 수정 다이얼로그를 새로 띄웠을 경우 기존 데이터를 모두 지움
- */
-function _clearModal() {
-    let forms = $('#id-modal-updateitem').find('form');
-    for(let i = 0; i < forms.length; i++) {
-        forms[i].reset();
-    }
-    $('#id-updateitem-id').val("");
-}
-
-/*-----------------------------------------------------------------------------
- * 기존에 작동된 validation 결과를 삭제함
- */
-function _eraseErrorLabel() {
-    let error_labels =  $('#id-modal-updateitem').find('label.error');
-    for(let i = 0; i < error_labels.length; i++) {
-        $(error_labels[i]).remove();
-    }
-}
-
-
-/*-----------------------------------------------------------------------------
- * 의상 등록 다이얼로그를 새로 띄웠을 경우 기존 데이터를 모두 지움
- */
-function _clearModal() {
-    let forms = $('#id-modal-updateitem').find('form');
-    for(let i = 0; i < forms.length; i++) {
-        forms[i].reset();
-    }
-
-    let imgs = $('#id-modal-updateitem').find('img');
-    for(let i = 0; i < imgs.length; i++) {
-        $(imgs[i]).attr('src', "/static/hipdrobe/img/no-image.png");
-    }
-
-    $('#id-updateitem-url').val("");
-}
-
-
-/*-----------------------------------------------------------------------------
- * 최종 업로드 버튼
- */
-function _setUploadTrigger() {
-    $('#id-btn-updateitem').click(function(event){
-        event.stopPropagation();
-        event.preventDefault();
-
-        // 버튼이 클릭되면 validator로 결정권을 넘김
-        $('#id-form-updateitem').submit();
     });
 }
 
@@ -223,7 +91,7 @@ function _setUploadTrigger() {
  *  https://jqueryvalidation.org/
  *      
  */
-function _makeValidator() {
+function _makeValidator_clothes_update() {
     $("#id-form-updateitem").validate({
         rules: {
           part: {required: true },
@@ -239,25 +107,5 @@ function _makeValidator() {
         success: function (e) {
             //ToDo: Nonthing To Do...
         }
-    });
-
-    $.extend( $.validator.messages, { 
-        required: "필수 항목입니다.", 
-        remote: "항목을 수정하세요.", 
-        email: "유효하지 않은 E-Mail주소입니다.", 
-        url: "유효하지 않은 URL입니다.", 
-        date: "올바른 날짜를 입력하세요.", 
-        dateISO: "올바른 날짜(ISO)를 입력하세요.", 
-        number: "유효한 숫자가 아닙니다.", 
-        digits: "숫자만 입력 가능합니다.", 
-        creditcard: "신용카드 번호가 바르지 않습니다.", 
-        equalTo: "같은 값을 다시 입력하세요.", 
-        extension: "올바른 확장자가 아닙니다.", 
-        maxlength: $.validator.format( "{0}자를 넘을 수 없습니다. " ), 
-        minlength: $.validator.format( "{0}자 이상 입력하세요." ), 
-        rangelength: $.validator.format( "문자 길이가 {0} 에서 {1} 사이의 값을 입력하세요." ), 
-        range: $.validator.format( "{0} 에서 {1} 사이의 값을 입력하세요." ),
-        max: $.validator.format( "{0} 이하의 값을 입력하세요." ), 
-        min: $.validator.format( "{0} 이상의 값을 입력하세요." ) 
     });
 }
