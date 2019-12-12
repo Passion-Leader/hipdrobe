dd6dcacba95af3b209f9bd3cd69d08bb4c46bcfc
codeorchord <codeorchord@gmail.com>
2019-12-12 15:01:36 +0900

    Heroku 배포 준비

--- src/web/config/settings.py
+++ src/web/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o!2e6d#f&&a3o*9nirtzqc)2ex38!kvz5v*#0j^20=hx2awgnv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -150,3 +151,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'hipdrobe.User'
 SILENCED_SYSTEM_CHECKS = ["auth.W004"]
+
+import django_heroku
+django_heroku.settings(locals())
